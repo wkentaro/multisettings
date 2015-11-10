dd947469b993d5e4971b3d1f9d6bd2eb0bdb96a7
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-10 15:09:22 +0900

    Add main func

--- multisettings/__init__.py
+++ multisettings/__init__.py
@@ -11,35 +11,40 @@ import yaml
 __version__ = '0.0.1'
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('user', nargs='?', default='default',
-                    help='user setting you set')
-parser.add_argument('-c', '--config-file', help='config file.')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user', nargs='?', default='default',
+                        help='user setting you set')
+    parser.add_argument('-c', '--config-file', help='config file.')
+    args = parser.parse_args()
 
-active_user = args.user
-config_file = args.config_file
+    active_user = args.user
+    config_file = args.config_file
 
-if config_file is None:
-    config_file = os.path.expanduser('~/.multisettings.yaml')
+    if config_file is None:
+        config_file = os.path.expanduser('~/.multisettings.yaml')
 
-config = yaml.load(open(config_file))
+    config = yaml.load(open(config_file))
 
-for usr, files in config.items():
-    for default, specific in files.items():
-        if os.path.exists(specific):
-            continue
-        if os.path.exists(default):
-            yn = raw_input('Can I rename file {0} -> {1}? [y/n]: '.format(default, default + '.default'))
-            if yn.lower() != 'y':
+    for usr, files in config.items():
+        for default, specific in files.items():
+            if os.path.exists(specific):
                 continue
-            shutil.move(default, default + '.default')
-            os.symlink(default + '.default', specific)
+            if os.path.exists(default):
+                yn = raw_input('Can I rename file {0} -> {1}? [y/n]: '.format(default, default + '.default'))
+                if yn.lower() != 'y':
+                    continue
+                shutil.move(default, default + '.default')
+                os.symlink(default + '.default', specific)
 
-config = ConfigParser.RawConfigParser()
-config.add_section('core')
-config.set('core', 'active_user', active_user)
+    config = ConfigParser.RawConfigParser()
+    config.add_section('core')
+    config.set('core', 'active_user', active_user)
 
-dotfile = os.path.expanduser('~/.multisettings')
-with open(dotfile, 'wb') as f:
-    config.write(f)
+    dotfile = os.path.expanduser('~/.multisettings')
+    with open(dotfile, 'wb') as f:
+        config.write(f)
+
+
+if __name__ == '__main__':
+    main()
